601908329c43cb56024c09355c9fa92da3e32fc9
hoatv02 <105353827+hoatv02@users.noreply.github.com>
2022-11-17 09:05:02 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
 import {Routes,Route} from 'react-router-dom'
-import Home from './pages/PagesClient/Home/Home'
-import Header from './component/Client/Header/Header'
-import LayoutClient from './layout/LayoutClient/LayoutClient'
-import HeaderNoSlider from './component/Client/HeaderNoSlider/HeaderNoSlider'
-import Product from './pages/PagesClient/MenuProduct/MenuProduct'
-import About from './pages/PagesClient/About/About'
 import Admin from './Routes/RoutesAdmin/Admin'
 import Client from './Routes/RoutesClient/Client'
 function App() {
